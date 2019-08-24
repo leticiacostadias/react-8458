581ff89f7bfeeda90d09f4c797e2137c081a0e83
Letícia Costa <lcosta@verios.com.br>
2019-08-24 12:03:53 -0300

    adicionando lógica do login

--- src/pages/LoginPage/index.js
+++ src/pages/LoginPage/index.js
@@ -5,7 +5,38 @@ import Widget from '../../components/Widget'
 import './loginPage.css'
 
 class LoginPage extends Component {
+  // handleLogin() {} -> precisa de um bind
+  handleLogin = (evento) => {
+    evento.preventDefault();
+
+    const login = this.refs.login.value;
+    const senha = this.refs.senha.value;
+
+    // XMLHttpRequest
+    // IE 6 -> axios
+    fetch('https://twitelum-api.herokuapp.com/login', {
+      method: 'POST',
+      body: JSON.stringify({ login, senha })
+    }).then(async (resposta) => {
+      // console.log('resposta:', resposta);
+      // console.log('resposta.body:', resposta.body); // Readable Stream
+
+      // resposta.status === 200
+      // if (!resposta.ok) throw new Error();
+      const data = await resposta.json();
+
+      if (resposta.ok) {
+        localStorage.setItem('token', data.token);
+        this.props.history.push('/');
+      } else {
+        console.log(data);
+      }
+    }).catch(err => console.log(err));
+  }
+
   render() {
+    // console.log(this.props.history);
+
     return (
       <Fragment>
         <Cabecalho />
@@ -13,14 +44,15 @@ class LoginPage extends Component {
           <div className="container">
             <Widget>
               <h2 className="loginPage__title">Seja bem vindo!</h2>
-              <form className="loginPage__form" action="/">
+
+              <form className="loginPage__form" action="/" onSubmit={this.handleLogin}>
                 <div className="loginPage__inputWrap">
                   <label className="loginPage__label" htmlFor="login">Login</label>
-                  <input className="loginPage__input" type="text" id="login" name="login" />
+                  <input ref="login" className="loginPage__input" type="text" id="login" name="login" />
                 </div>
                 <div className="loginPage__inputWrap">
                   <label className="loginPage__label" htmlFor="senha">Senha</label>
-                  <input className="loginPage__input" type="password" id="senha" name="senha" />
+                  <input ref="senha" className="loginPage__input" type="password" id="senha" name="senha" />
                 </div>
                 {/* <div className="loginPage__errorBox">
                   Mensagem de erro!
