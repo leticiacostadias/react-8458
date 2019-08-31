8d7ae9d668b73b039e4f5c9efbe6cd05426e16f7
Letícia Costa <lcosta@verios.com.br>
2019-08-31 09:42:26 -0300

    adicionando services

--- src/pages/LoginPage/index.js
+++ src/pages/LoginPage/index.js
@@ -4,6 +4,8 @@ import Widget from '../../components/Widget'
 import If from '../../components/If';
 
 import { NotificacaoContext } from './../../contexts/notificacao';
+// import { logar } from '../../services/login';
+import * as LoginService from '../../services/login';
 
 import './loginPage.css'
 
@@ -21,28 +23,40 @@ class LoginPage extends Component {
     const login = this.refs.login.value;
     const senha = this.refs.senha.value;
 
+    LoginService.logar(login, senha)
+      .then(({ data, respostaOk }) => {
+        if (respostaOk) {
+          localStorage.setItem('token', data.token);
+          this.context.setMensagem('Login efetuado com sucesso! Bem vindo!');
+          this.props.history.push('/');
+        } else {
+          // console.log(data);
+          this.setState({ errorMessage: data.message });
+        }
+      }).catch(err => console.log(err));
+
     // XMLHttpRequest
     // IE 6 -> axios
-    fetch('https://api-twitelum.herokuapp.com/login', {
-      method: 'POST',
-      body: JSON.stringify({ login, senha })
-    }).then(async (resposta) => {
-      // console.log('resposta:', resposta);
-      // console.log('resposta.body:', resposta.body); // Readable Stream
+    // fetch('https://api-twitelum.herokuapp.com/login', {
+    //   method: 'POST',
+    //   body: JSON.stringify({ login, senha })
+    // }).then(async (resposta) => {
+    //   // console.log('resposta:', resposta);
+    //   // console.log('resposta.body:', resposta.body); // Readable Stream
 
-      // resposta.status === 200
-      // if (!resposta.ok) throw new Error();
-      const data = await resposta.json();
+    //   // resposta.status === 200
+    //   // if (!resposta.ok) throw new Error();
+    //   const data = await resposta.json();
 
-      if (resposta.ok) {
-        localStorage.setItem('token', data.token);
-        this.context.setMensagem('Login efetuado com sucesso! Bem vindo!');
-        this.props.history.push('/');
-      } else {
-        // console.log(data);
-        this.setState({ errorMessage: data.message });
-      }
-    }).catch(err => console.log(err));
+    //   if (resposta.ok) {
+    //     localStorage.setItem('token', data.token);
+    //     this.context.setMensagem('Login efetuado com sucesso! Bem vindo!');
+    //     this.props.history.push('/');
+    //   } else {
+    //     // console.log(data);
+    //     this.setState({ errorMessage: data.message });
+    //   }
+    // }).catch(err => console.log(err));
   }
 
   render() {
---
+++ src/services/login.js
@@ -0,0 +1,18 @@
+export function logar(login, senha) {
+  return fetch('https://api-twitelum.herokuapp.com/login', {
+      method: 'POST',
+      body: JSON.stringify({ login, senha })
+    }).then(async (resposta) => {
+      // console.log('resposta:', resposta);
+      // console.log('resposta.body:', resposta.body); // Readable Stream
+
+      // resposta.status === 200
+      // if (!resposta.ok) throw new Error();
+      const data = await resposta.json();
+
+      return {
+        data,
+        respostaOk: resposta.ok
+      };
+    });
+}
